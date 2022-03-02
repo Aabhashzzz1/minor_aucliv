05a206f7d2ea47453a41ac9f843b8ed5e5c95ef7
malviya_aabhash <55883195+Aabhashzzz1@users.noreply.github.com>
2022-03-02 19:04:26 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>logout</title>
 </head>
 <body>
     <?php
     session_start();
     session_destroy();
-    header("location:http://localhost/minor2/index.php")
+    header("location:index.php")
     
     ?>
 </body>
